8fa09177d4605465819edddf73fc7fa255539d21
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-12-15 08:12:17 +0500

    API integration and server settings

--- server/index.js
+++ server/index.js
@@ -1,25 +1,34 @@
+// import express from 'express';
 const express = require('express');
 const mongoose = require('mongoose');
+// import { userModel } from './models/user.models';
+const userModel = require('./models/user.models');
+// import cors from 'cors';
+const cors = require('cors');
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/crud');
 
-const UserSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-})
-const UserModel = mongoose.model("users", UserSchema);
+app.use(cors());
+app.use(express.json());
 
+mongoose.connect("mongodb://localhost:27017/crud");
 
 app.get('/getUsers', (req, res) => {
-    UserModel.find({}).then((users) => {
+    userModel.find({}).then((users) => {
         res.json(users);
     }).catch((err) => {
-        console.log(err);
-    })
-})
+        res.json(err);
+    });
+});
+
+app.post('/createUser', async (req, res) => {
+    const user = req.body;
+    const newUser = new userModel(user);
+    await newUser.save();
+    res.json(user);
+});
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3001, () => {
+    console.log("server is running");
 })
